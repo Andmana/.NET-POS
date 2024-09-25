f588a3cfebfd3c6f92f94c9562372fe59b2789dd
cllainn <cllainn@gmail.com>
2024-09-25 14:53:25 +0700

    Create new dummy page
    Coba coba buat page baru

---
+++ Xpos341/Controllers/DummyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Xpos341.Controllers
+{
+    public class DummyController : Controller
+    {
+        public IActionResult Bootcamp()
+        {
+            return View();
+        }
+    }
+}
