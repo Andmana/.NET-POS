cf02e763d4b8696798ee20dc53f0ea6186381f63
cllainn <cllainn@gmail.com>
2024-09-26 11:13:18 +0700

    Init category try controller

---
+++ Xpos341/Controllers/CategoryTryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Xpos341.Controllers
+{
+    public class CategoryTryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
