1397f6d4369cfae15d5bd254ce7df682fac76ef7
cllainn <cllainn@gmail.com>
2024-09-27 17:26:00 +0700

    Sql Table

--- SQLquery/SQLQuery1.sql
+++ SQLquery/SQLQuery1.sql
@@ -20,3 +20,22 @@ select * from TblCategory;
 insert into TblCategory(NameCategory, Description, IsDelete, CreateBy, CreateDate, UpdateBy,UpdateDate) values
 ('Drink', 'Desc of drinks', 0, 1, GETDATE(), null, null),
 ('Food', 'Desc of Food', 0, 2, GETDATE(), null, null);
+
+drop table if exists TblVariant;
+create table TblVariant
+(
+	Id				int primary key identity(1,1),
+	IdCategory		int not null,
+	NameVariant		varchar(50) not null,
+	Description		varchar(max),
+	IsDelete		bit,
+	CreateBy		int not null,
+	CreateDate		datetime not null,
+	UpdateBy		int,
+	UpdateDate		datetime
+)
+
+
+
+
+
