4ad8df1bac6e5f83b10f1126b9ecc7e797f07130
andmana <iam.andmana@gmail.com>
2024-09-30 09:57:13 +0700

    Init variant try controller and view, also change sidebar tab

---
+++ Xpos341/Controllers/VariantTryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Xpos341.Controllers
+{
+    public class VariantTryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
