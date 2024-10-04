520747f290eda4d4811fc7afa5a6f74e1abf6964
cllainn <cllainn@gmail.com>
2024-10-04 11:24:42 +0700

    Init product view controller

---
+++ Xpos341/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
+using Xpos341.Services;
+
+namespace Xpos341.Controllers
+{
+    public class ProductController : Controller
+    {
+        private CategoryService categoryService;
+        private VariantService variantService;
+        private ProductService productService;
+
+        private int idUser = 1;
+
+        public ProductController(CategoryService _categoryService, 
+                                 VariantService _variantService,
+                                 ProductService _productService)
+        {
+            categoryService = _categoryService;
+            variantService = _variantService;
+            productService = _productService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Xpos341/Services/ProductService.cs
@@ -0,0 +1,18 @@
+﻿using xpos341.viewmodels;
+
+namespace Xpos341.Services
+{
+    public class ProductService
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private IConfiguration configuration;
+        private string RouteAPI = "";
+        private VMResponse response = new VMResponse();
+        public ProductService(IConfiguration _configuration)
+        {
+            configuration = _configuration;
+            RouteAPI = configuration["RouteAPI"];
+        }
+    }
+
+}
